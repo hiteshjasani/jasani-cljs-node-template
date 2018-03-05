9a517d8f47bc78347a2735e88ecd93a0e282dfba
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-03-05 13:15:11 -0500

    Update to v0.1.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject jasani-cljs-node/lein-template "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject jasani-cljs-node/lein-template "0.1.1"
+  :description "A leiningen template to generate a simple cljs on nodejs app."
+  :url "https://github.com/hiteshjasani/jasani-cljs-node-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
