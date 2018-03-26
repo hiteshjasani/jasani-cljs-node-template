acd94c312258f0248d1df7765e2fb00fd2ab3fca
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-03-26 09:50:10 -0400

    Upgrade to clojurescript 1.10.238

--- resources/leiningen/new/jasani_cljs_node/project.clj
+++ resources/leiningen/new/jasani_cljs_node/project.clj
@@ -1,6 +1,6 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure       "1.9.0"]
-                 [org.clojure/clojurescript "1.10.64"]
+                 [org.clojure/clojurescript "1.10.238"]
                  [org.clojure/core.async    "0.4.474"]
                  ]
   :plugins [[lein-npm       "0.6.2"]
