97482b3d4fbdecd04fab3080df19c478305056e2
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-03-26 13:03:21 -0400

    Enable nREPL with CIDER

--- resources/leiningen/new/jasani_cljs_node/project.clj
+++ resources/leiningen/new/jasani_cljs_node/project.clj
@@ -4,10 +4,12 @@
                  [org.clojure/core.async    "0.4.474"]
                  ]
   :plugins [[lein-npm       "0.6.2"]
-            [lein-cljsbuild "1.1.7"]]
+            [lein-cljsbuild "1.1.7"]
+            [cider/cider-nrepl "0.17.0-SNAPSHOT"]]
   :cljsbuild
   {:builds
-   [{:source-paths ["src"]
+   [{:id "dev"
+     :source-paths ["src"]
      :compiler {:output-to     "target/{{name}}/{{name}}.js"
                 :output-dir    "target/{{name}}"
                 :main          "{{name}}.core"
@@ -15,4 +17,10 @@
                 ;; Manage nodejs dependencies here
                 :npm-deps      {"string-template" "1.0.0"}
                 :install-deps  true
-                :optimizations :none}}]})
+                :optimizations :none}}
+    ]}
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.2"]
+                                  [org.clojure/tools.nrepl "0.2.10"]]
+                   :repl-options
+                   {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+  )
